8c6099154b7e331c0661b970e5a48cd1d2c35686
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-09-26 17:33:49 +0300

    Initial commit

---
+++ RabbitMqConsumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory rabbitFactory = new() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+            using(IConnection connection = rabbitFactory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "my_first_queue",
+                        durable: true
+                        , exclusive: false
+                        , autoDelete: false
+                        , arguments: null);
+
+                    EventingBasicConsumer consumer = new(channel);
+
+                    consumer.Unregistered += (sender, e) => Console.WriteLine("[x] Consumer unregistered");
+                    consumer.Shutdown += (sender, e) => Console.WriteLine("[x] Consumer shutdown");
+                    consumer.Registered += (sender, e) => Console.WriteLine("[x] Consumer registered");
+                    consumer.ConsumerCancelled += (sender, e) => Console.WriteLine("[x] Consumer canceled.");
+
+                    consumer.Received += (sender, e) =>
+                    {
+                        byte[] body = e.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($"[x] Received {message}");
+                    };
+
+                    channel.BasicConsume(
+                        queue: "my_first_queue",
+                        autoAck: true,
+                        consumer: consumer);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMqSamples/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory rabbitFactory = new() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+            using(IConnection connection = rabbitFactory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue:"my_first_queue",
+                        durable: true
+                        , exclusive: false
+                        , autoDelete: false
+                        , arguments: null);
+
+                    string message = "Hello RabbitMq!";
+                    byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: string.Empty
+                        ,routingKey: "my_first_queue"
+                        , basicProperties: null
+                        ,body: body);
+
+                    Console.WriteLine($"[x] Sent message {message}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
