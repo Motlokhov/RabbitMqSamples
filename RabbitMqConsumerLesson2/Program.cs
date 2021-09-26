766ab67a6567ec562924c0e3e1ebe1a952502030
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-09-26 20:43:36 +0300

    Added lesson2

---
+++ RabbitMqConsumerLesson2/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMqConsumerLesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, e) =>
+            {
+                byte[] body = e.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine($"[x] Message: {message}");
+
+                Thread.Sleep(2000);
+
+                Console.WriteLine($"[x] Done. DeliveryTag:{e.DeliveryTag}");
+                ((EventingBasicConsumer)sender).Model.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume("task_queue", false, consumer);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RabbitMqProducerLesson2/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMqProducerLesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+            using(IConnection connection = factory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "task_queue",
+                        durable: true
+                        , exclusive: false
+                        , autoDelete: false
+                        , arguments: null);
+
+                    IBasicProperties properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;//save message almost 100% probability on server (save for reboot)
+
+                    int i = 1;
+                    while(true)
+                    {
+                        string message = $"[x] Produced message {i++}";
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(
+                            exchange: string.Empty,
+                            routingKey: "task_queue",
+                            basicProperties: properties,
+                            body: body);
+
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+    }
+}
