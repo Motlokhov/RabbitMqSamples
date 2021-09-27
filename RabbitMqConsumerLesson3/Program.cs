f9b4ac676027509cf65db982b261e71365d75bdb
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-09-27 21:33:10 +0300

    Added lesson 3

---
+++ RabbitMqConsumerLesson3/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqConsumerLesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+            using(IConnection connection = factory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+                    string queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue: queueName, exchange: "logs", routingKey: string.Empty);
+
+                    Console.WriteLine("[*] Waiting for logs.");
+
+                    EventingBasicConsumer consumer = new(channel);
+                    consumer.Received += (sender, e) =>
+                    {
+                        byte[] body = e.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($"[x] {message}");
+                    };
+
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMqProducerLesson3/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMqProducerLesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+            using(IConnection connection = factory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                    int i = 1;
+                    while(true)
+                    {
+                        string message = $"Message {i++}";
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "logs", routingKey: string.Empty, body: body);
+                        Console.WriteLine($"[x] Sent {message}");
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+    }
+}
