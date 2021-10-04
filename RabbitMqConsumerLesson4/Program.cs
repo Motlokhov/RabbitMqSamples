855b1e59d67d313fe8d318b1361b48fedf873170
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-10-04 21:39:40 +0300

    Added lesson4

---
+++ RabbitMqConsumerLesson4/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMqConsumerLesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+            using(IConnection connection = factory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    string queueName = "log_console";
+
+                    EventingBasicConsumer consumer = new(channel);
+                    consumer.Received += (sender, e) =>
+                    {
+                        byte[] body = e.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(body);
+                        ((EventingBasicConsumer)sender).Model.BasicAck(deliveryTag: e.DeliveryTag,multiple:false);
+                        Console.WriteLine($"[x] {message} write to console. Route key:{e.RoutingKey}");
+                    };
+                    channel.BasicConsume(queueName, false,consumer);
+
+                    string queueName1 = "log_fileSave";
+
+                    EventingBasicConsumer consumer1 = new(channel);
+                    consumer1.Received += (sender, e) =>
+                    {
+                        byte[] body = e.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(body);
+                        ((EventingBasicConsumer)sender).Model.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+                        Console.WriteLine($"[x] {message} write to file. Route key:{e.RoutingKey}");
+                    };
+                    channel.BasicConsume(queueName1, false, consumer1);
+
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMqProducerLesson4/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMqProducerLesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+            using(IConnection connection = factory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    string logFileQueueName = "log_fileSave";
+                    string logConsoleQueueName = "log_console";
+                    string exchangeName = "direct_logs";
+
+                    channel.QueueDeclare(logFileQueueName, true, false, false);
+                    channel.QueueDeclare(logConsoleQueueName, true, false, false);
+                    channel.ExchangeDeclare(exchangeName, type: ExchangeType.Direct);
+                    //to file
+                    channel.QueueBind(logFileQueueName, exchangeName, "error");
+                    //to console
+                    channel.QueueBind(logConsoleQueueName, exchangeName, "error");
+                    channel.QueueBind(logConsoleQueueName, exchangeName, "info");
+                    channel.QueueBind(logConsoleQueueName, exchangeName, "warning");
+
+                    int i = 1;
+                    while(true)
+                    {
+                        string message = $"Message {i++} is ";
+                        string routeKey = "info";
+                        if(i % 10 == 0)
+                            routeKey = "error";
+                        else if
+                            (i % 3 == 0)
+                            routeKey = "warning";
+
+                        message += routeKey;
+
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchangeName, routeKey, null, body);
+                        Console.WriteLine($"[x] {message}");
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+    }
+}
