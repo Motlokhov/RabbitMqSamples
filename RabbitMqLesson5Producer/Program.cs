9581db2a41cfa276c8dd30cd6c524cd470678b4d
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-10-05 21:09:45 +0300

    Addded lesson 5(topic)

---
+++ RabbitMqLesson5Producer/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqProducerLesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+            using(IConnection connection = factory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    string exchangeName = "topic_exchange_example";
+                    channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, true);
+
+                    string queue1 = "speed.orange.species";
+                    string queue2 = "speed.colour.rabbit";
+                    string queue3 = "quick.colour.swine";
+                    channel.QueueDeclare(queue1, true,false,false);
+                    channel.QueueDeclare(queue2, true, false, false);
+                    channel.QueueDeclare(queue3, true, false, false);
+
+                    channel.QueueBind(queue1, exchangeName, "*.orange.*");
+                    channel.QueueBind(queue2, exchangeName, "*.*.rabbit");
+                    channel.QueueBind(queue2, exchangeName, "lazy.#");
+                    channel.QueueBind(queue3, exchangeName, "quick.*.swine");
+
+                    string[] messages =
+                    {
+                        "quick.orange.rabbit",// q1,q2
+                        "lazy.orange.elephant", //q1,q2
+                        "quick.orange.fox", //q1
+                        "lazy.brown.fox", //q2
+                        "lazy.pink.rabbit", //q2 (matching 2 times, but will be send once)
+                        "quick.brown.fox", //no matching
+                        "lazy.orange.male.rabbit", //q2
+                        "quick.brown.swine" //q3
+                    };
+
+                    foreach(string message in messages)
+                    {
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchangeName, message, null, body);
+                        Console.WriteLine($"[x] Message {message} is sent.");
+                    }
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMqconsumerLesson5/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqconsumerLesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+            using(IConnection connection = factory.CreateConnection())
+            {
+                using(IModel channel = connection.CreateModel())
+                {
+                    string exchangeName = "topic_exchange_example";
+
+                    string queue1 = "speed.orange.species";
+                    string queue2 = "speed.colour.rabbit";
+                    string queue3 = "quick.colour.swine";
+
+                    EventingBasicConsumer consumer = new(channel);
+                    consumer.Received += (sender, e) =>
+                    {
+                        string message = Encoding.UTF8.GetString(e.Body.ToArray());
+                        Console.WriteLine($"[x] Message {message} from {queue1}({e.RoutingKey},{e.DeliveryTag},{e.ConsumerTag})");
+                        ((EventingBasicConsumer)sender).Model.BasicAck(e.DeliveryTag, false);
+                    };
+                    channel.BasicConsume(queue1, false, consumer);
+
+                    EventingBasicConsumer consumer2 = new(channel);
+                    consumer2.Received += (sender, e) =>
+                    {
+                        string message = Encoding.UTF8.GetString(e.Body.ToArray());
+                        Console.WriteLine($"[x] Message {message} from {queue2}({e.RoutingKey},{e.DeliveryTag},{e.ConsumerTag})");
+                        ((EventingBasicConsumer)sender).Model.BasicAck(e.DeliveryTag, false);
+                    };
+                    channel.BasicConsume(queue2, false, consumer2);
+
+                    EventingBasicConsumer consumer3 = new(channel);
+                    consumer3.Received += (sender, e) =>
+                    {
+                        string message = Encoding.UTF8.GetString(e.Body.ToArray());
+                        Console.WriteLine($"[x] Message {message} from {queue3}({e.RoutingKey},{e.DeliveryTag},{e.ConsumerTag})");
+                        ((EventingBasicConsumer)sender).Model.BasicAck(e.DeliveryTag, false);
+                    };
+                    channel.BasicConsume(queue3, false, consumer3);
+
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
